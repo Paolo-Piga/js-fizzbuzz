33ffbc3a47e9edd60904d8b87d035139a8a6ac53
DecimTheJudge <paolo1piga@gmail.com>
2024-06-06 17:38:24 +0200

    continuo

--- js/main.js
+++ js/main.js
@@ -7,17 +7,17 @@ for (i =1; i <= 100; i++){
 
     // multipli di 3
     if (i == 3 || i%3==0) {
-        
+        console.log("FIZZ");
     }
     
     // multipli di 5
-    if (i == 5 || i%5==0){
-
+    else if (i == 5 || i%5==0) {
+        console.log("BUZZ");
     }
     
     // multipli di 3 e 5
     if ((i == 3 || i%3==0) && (i == 5 || i%5==0)) {
-
+        console.log("FIZZBUZZ");
     }
 }
 
