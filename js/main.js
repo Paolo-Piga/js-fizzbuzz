1ad0c14c700df624bb232ae2d1ad01059a752a96
DecimTheJudge <paolo1piga@gmail.com>
2024-06-06 17:05:23 +0200

    if dei vari multipli

--- js/main.js
+++ js/main.js
@@ -4,4 +4,22 @@ const ul = document.querySelector(".listona");
 // creo il ciclo per arrivare a 100
 for (i =1; i <= 100; i++){
     const li = document.createElement("li");
+
+    // multipli di 3
+    if (i == 3 || i%3==0) {
+        
+    }
+    
+    // multipli di 5
+    if (i == 5 || i%5==0){
+
+    }
+    
+    // multipli di 3 e 5
+    if ((i == 3 || i%3==0) && (i == 5 || i%5==0)) {
+
+    }
 }
+
+
+// && (AND) || (OR)
