9232ef387d5595582b5bd4b71077d32fdb8f6e9b
DecimTheJudge <paolo1piga@gmail.com>
2024-06-06 16:42:44 +0200

    inizio

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// creo una variabile per la la lista ul
+const ul = document.querySelector(".listona");
+
+// creo il ciclo per arrivare a 100
+for (i =1; i <= 100; i++){
+    const li = document.createElement("li");
+}
